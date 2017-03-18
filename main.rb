05ccf0b544ff360d6478de95113d2cc0af4dc146
Kieran Eglin <kieran.eglin@gmail.com>
2017-03-17 20:44:24 -0700

    templates now use correct colours as provided by content

--- main.rb
+++ main.rb
@@ -1,6 +1,14 @@
 require_relative 'sync-template'
 
-content = '{ "name": "Peachy", "primary_color": "#b95289", "accent_color": "#fbb2b3", "highlight_color": "#59c99d", "primary_text_color": "#697c8f", "secondary_text_color": "#8d8d8d", "window_color": "#e4e4e4", "content_color": "#ffffff", "auto_subreddit_themes": false }'
+content = '{ "name": "Peachy",
+  "primary_color": "#b95289",
+  "accent_color": "#fbb2b3",
+  "highlight_color": "#59c99d",
+  "primary_text_color": "#697c8f",
+  "secondary_text_color": "#8d8d8d",
+  "window_color": "#e4e4e4",
+  "content_color": "#ffffff",
+  "auto_subreddit_themes": false }'
 
 s = SyncTemplate.new content
 s.render
